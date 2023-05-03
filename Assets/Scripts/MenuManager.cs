666a32dc8bb925c19913eebf8b5a46ad757f4b40
ainetta12 <120013205+ainetta12@users.noreply.github.com>
2023-05-03 09:25:23 +0200

    volumen

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private AudioSource bgmSource;
+    private AudioSource sfxSource;
+
+    private void Awake() 
+    {
+        bgmSource = GameObject.Find("SoundManager").GetComponent<AudioSource>();
+        sfxSource = GameObject.Find("SFXManager").GetComponent<AudioSource>();
+    }
+    
     public void Play() 
     {
         SceneManager.LoadScene(1);
@@ -13,6 +22,16 @@ public class MenuManager : MonoBehaviour
     public void ExitGame ()
     {
         Application.Quit();
+    }
+
+    public void MusicVolume(float value)
+    {
+        bgmSource.volume = value;
+    }
+     
+    public void SFXVolume(float value)
+    {
+        sfxSource.volume = value;
     }
 
     public void Menu()
